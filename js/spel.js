095aed66ec04103d6427f6d7425f305b818f5083
ArneVanhyfte <arne.vanhyfte@student.odisee.be>
2022-04-29 12:32:32 +0200

    Dobbelsteen js en werkend in html(zonder opmaak)

--- js/spel.js
+++ js/spel.js
@@ -1,18 +1,46 @@
 "use strict";
 
-(function (){
+(function () {
 
-
-
-   /* kleur boxes aanpassen indien dynamisch
-    const color = ()=>{
-        const boxes = document.querySelectorAll('.box');
-        for (let t =0 ; t<boxes.length ; t++){
-            if (t%2===0){
-                boxes[t].classList.add("red");
-            }else {
-                boxes[t].classList.add("white");
-            }
+    class Dobbelsteen {
+        constructor(min, max) {
+            this.min = min;
+            this.max = max;
+            this.worp = min;
         }
-    }*/
+        gooi() {
+            this.worp = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+        }
+
+        geefLaatsteWorp() {
+            return this.worp;
+        }
+    }
+    const dobbelsteen = new Dobbelsteen(1, 6);
+    const btn = document.querySelector('.btn');
+    btn.classList.add("red");
+    const geworpen = document.querySelector('.geworpen');
+
+    const worp = () => {
+        let worp;
+
+        dobbelsteen.gooi();
+        worp= dobbelsteen.geefLaatsteWorp()
+        geworpen.innerText = worp;
+    }
+    btn.addEventListener('click', function () {
+            worp();
+    });
+
+    /* kleur boxes aanpassen indien dynamisch
+     const color = ()=>{
+         const boxes = document.querySelectorAll('.box');
+         for (let t =0 ; t<boxes.length ; t++){
+             if (t%2===0){
+                 boxes[t].classList.add("red");
+             }else {
+                 boxes[t].classList.add("white");
+             }
+         }
+     }*/
 })()
