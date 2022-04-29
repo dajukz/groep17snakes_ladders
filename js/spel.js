ae5797330caff662ee0838dc77bd303265123206
ArneVanhyfte <arne.vanhyfte@student.odisee.be>
2022-04-29 11:44:59 +0200

    grid v2 kleur css

--- js/spel.js
+++ js/spel.js
@@ -1,16 +1,6 @@
 "use strict";
 
 (function (){
-    const color = ()=>{
-        const boxes = document.querySelectorAll('.box');
-        for (let t =0 ; t<boxes.length ; t++){
-            if (t%2===0){
-                boxes[t].classList.add("red");
-            }else {
-                boxes[t].classList.add("white");
-            }
-        }
-    }
-color();
+
 
 })()
