f5bb39a1de85c28902b680272238f0251552b6e8
ArneVanhyfte <arne.vanhyfte@student.odisee.be>
2022-05-06 10:02:53 +0200

    Basic pawn move v1

--- js/spel.js
+++ js/spel.js
@@ -8,6 +8,7 @@
             this.max = max;
             this.worp = min;
         }
+
         gooi() {
             this.worp = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
         }
@@ -16,21 +17,27 @@
             return this.worp;
         }
     }
+
     const dobbelsteen = new Dobbelsteen(1, 6);
     const btn = document.querySelector('.btn');
-    btn.classList.add("red");
     const geworpen = document.querySelector('.geworpen');
+    const boxes = document.querySelectorAll('.box');
+    const pawns = document.querySelectorAll('.pawns');
+    let index = 0;
 
     const gooien = () => {
         dobbelsteen.gooi();
+        geworpen.innerHTML = dobbelsteen.geefLaatsteWorp();
         return dobbelsteen.geefLaatsteWorp();
     }
     btn.addEventListener('click', function () {
-            let worp;
-            worp = gooien();
-            geworpen.innerHTML=(worp);
-
+        index += gooien();
+        movePawn(index, 1);
+        pawns[0].classList.add("red");
     });
+    const movePawn = (index, pion) => {
+        boxes[index-1].appendChild(pawns[pion]);
+    }
 
     /* kleur boxes aanpassen indien dynamisch
      const color = ()=>{
