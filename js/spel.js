92e37ce3ee582d88845c3aa51992465178c61d2c
dajukz <31631919+dajukz@users.noreply.github.com>
2022-05-13 12:40:03 +0200

    Indexpagina (#8) (#9)
    * html index v1
    
    * basis css index
    
    * keep up with main (#6)
    
    * Dev (#1)
    
    * test
    
    * roadmap document v1
    
    Co-authored-by: dajukz <s116456@ap.be>
    
    * grid v1
    
    * Test (#2)
    
    * test
    
    * roadmap document v1
    
    Co-authored-by: dajukz <s116456@ap.be>
    
    * grid v1
    css 'ingekort'
    
    * experimental
    
    * ...
    
    * grid v2
    
    * grid v2 kleur
    
    * grid v2 kleur css
    
    * center grid en comment js
    
    Co-authored-by: dajukz <s116456@ap.be>
    Co-authored-by: ArneVanhyfte <arne.vanhyfte@student.odisee.be>
    Co-authored-by: Captain-TCA <connor.abbeel@student.odisee.be>
    Co-authored-by: SiemenRanschaert <75678877+SiemenRanschaert@users.noreply.github.com>
    Co-authored-by: ArneVanhyfte <95035639+ArneVanhyfte@users.noreply.github.com>
    
    * static index page v1
    
    * responsive 50%
    
    * aanpassing footer
    
    * alles behalve footer responsive
    
    * alles behalve footer responsive #2
    
    * footer
    
    * footer
    
    * aria footer
    
    Co-authored-by: dajukz <s116456@ap.be>
    Co-authored-by: ArneVanhyfte <arne.vanhyfte@student.odisee.be>
    Co-authored-by: Captain-TCA <connor.abbeel@student.odisee.be>
    Co-authored-by: SiemenRanschaert <75678877+SiemenRanschaert@users.noreply.github.com>
    Co-authored-by: ArneVanhyfte <95035639+ArneVanhyfte@users.noreply.github.com>
    
    Co-authored-by: dajukz <s116456@ap.be>
    Co-authored-by: ArneVanhyfte <arne.vanhyfte@student.odisee.be>
    Co-authored-by: Captain-TCA <connor.abbeel@student.odisee.be>
    Co-authored-by: SiemenRanschaert <75678877+SiemenRanschaert@users.noreply.github.com>
    Co-authored-by: ArneVanhyfte <95035639+ArneVanhyfte@users.noreply.github.com>

--- js/spel.js
+++ js/spel.js
@@ -1,5 +1,22 @@
 "use strict";
 
+
+/*(function (){
+
+
+
+   /* kleur boxes aanpassen indien dynamisch
+    const color = ()=>{
+        const boxes = document.querySelectorAll('.box');
+        for (let t =0 ; t<boxes.length ; t++){
+            if (t%2===0){
+                boxes[t].classList.add("red");
+            }else {
+                boxes[t].classList.add("white");
+            }
+        }
+    }*/
+
 (function () {
 
     class Dobbelsteen {
